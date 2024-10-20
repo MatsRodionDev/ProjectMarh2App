9027c2cb128f98967f3100b26383fec5ba8f71c8
MatsRodionDev <162904839+MatsRodionDev@users.noreply.github.com>
2024-10-20 15:38:20 +0300

    create routing

---
+++ frontend/src/components/AppRouter.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Routes} from 'react-router-dom'
+import { workerRoutes, publishRoutes, managerRoutes, adminRoutes } from "../routes";
+
+
+const AppRouter = () => {
+    
+    return (
+        <Routes>
+        </Routes>
+    )
+}
+
+export default AppRouter;
---
+++ frontend/src/routes.js
@@ -0,0 +1,12 @@
+export const publicshRoutes = [
+    
+]
+export const workerRoutes = [
+    
+]
+export const managerRoutes = [
+    
+]
+export const adminRoutes = [
+    
+]
